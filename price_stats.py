3155e312f3dcf24f77272b2a0be9fd40156d4f78
Marcos Lima <marcoslima@Marcoss-MacBook-Pro.local>
2023-09-30 13:32:06 -0300

    first save

---
+++ price_stats.py
@@ -0,0 +1,14 @@
+
+# scripts on github
+
+import numpy as np
+import pandas as pd
+
+# if(!exists(price_data_dir)) price_data_dir="~/" price_data_dir="~/Zion/R/datacamp/python/"
+
+#cache from yahoo
+
+#cached price statistics storage
+
+# math indicators derived from price and volume
+
