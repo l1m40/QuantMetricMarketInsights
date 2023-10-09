e14285b1123d76e8128a430d74120e212571d728
Marcos Lima <marcoslima@Marcoss-MacBook-Pro.local>
2023-10-09 11:51:47 -0300

    .

--- cointegration_lab.py
+++ cointegration_lab.py
@@ -9,6 +9,13 @@ price_stats.price_data_dir="/home/data_lake_dir/"
 
 import pair_stats
 
+df = price_stats.cache_price_yfinance(["VALE3","PETR4"],True,True)
+df["Close"] = df["Adj Close"]
+pair_stats.cointegration_stats(df,"2023-10-05",160)
+
+
+
+
 df = price_stats.cache_price_data_path(["VALE3","PETR4"],price_stats.price_data_dir+"cache_data_extract1.csv")
 df["Close"] = df["Adj Close"]
 df["Close"] = np.log(df["Adj Close"])
